cbeadf2d7590a6d7eb6df6ef0ab71eeece6a34f1
kate <katepol14@gmail.com>
2012-04-26 21:52:41 +0400

    UnregisteredEditorException is back

--- jelisp/src/java/org/jetbrains/emacs4ij/jelisp/exception/UnregisteredEditorException.java
+++ jelisp/src/java/org/jetbrains/emacs4ij/jelisp/exception/UnregisteredEditorException.java
@@ -5,8 +5,8 @@ import org.jetbrains.emacs4ij.jelisp.JelispBundle;
 /**
  * Created with IntelliJ IDEA.
  * User: kate
- * Date: 4/2/12
- * Time: 12:43 PM
+ * Date: 4/25/12
+ * Time: 4:04 PM
  * To change this template use File | Settings | File Templates.
  */
 public class UnregisteredEditorException extends LispException {
