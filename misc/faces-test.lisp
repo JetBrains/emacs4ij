fc1a341157de37024b54afe382796e3744d116d4
Ekaterina Frolova <ekaterina.frolova@jetbrains.com>
2013-03-05 19:28:51 +0400

    elisp test file

---
+++ misc/faces-test.lisp
@@ -0,0 +1,27 @@
+;; This buffer is for notes you don't want to save, and for Lisp evaluation.
+;; If you want to create a file, visit that file with C-x C-f,
+;; then enter the text in that file's own buffer.
+
+(delete-other-windows "a")
+(window-buffer)
+(window-list)
+
+major-mode
+initial-major-mode
+(stringp 'system-type)
+(list-faces-display)
+get-text-property
+(text-properties-at 5)
+(zerop nil)
+?q
+
+internal-set-font-selection-order
+font-family-list
+(keywordp :height)
+set-face-attribute
+
+face-new-frame-defaults
+system-type
+delq
+
+(sort (face-list) #'string-lessp)
